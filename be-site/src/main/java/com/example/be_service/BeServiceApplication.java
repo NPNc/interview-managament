d02275284c3d34e5935556cf8276a5aea120a0e4
NPNc <NPNc>
2025-02-11 14:56:58 +0700

    Fix project

---
+++ be-site/src/main/java/com/example/be_service/BeServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.be_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScans;
+
+@SpringBootApplication
+public class BeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeServiceApplication.class, args);
+	}
+
+}
